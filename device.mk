d93439984bb21c1f04b13edc0b160e88e239e304
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2020-12-03 13:09:27 +0530

    platina: Include hardware/google/interfaces and hardware/google/pixel
     * Required by power-libperfmgr
    
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- device.mk
+++ device.mk
@@ -404,7 +404,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(DEVICE_PATH)
+    $(DEVICE_PATH) \
+    hardware/google/interfaces \
+    hardware/google/pixel
 
 # Tethering
 PRODUCT_PACKAGES += \
