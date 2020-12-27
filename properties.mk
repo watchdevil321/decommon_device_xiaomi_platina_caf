83fa8630fbc188bea20ee80cc4dbdc8da5c0bab4
Manish4586 <manish.n.manish45@gmail.com>
2020-12-28 02:01:51 +0530

    platina: props: enable vsync and disable backpressure
    * As seen with December patch some system apps including user apps started to lag (like screen tearing).
    * Enabling Vsync and disabling backpressure resolves the issue
    
    Signed-off-by: Manish4586 <manish.n.manish45@gmail.com>
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>

--- properties.mk
+++ properties.mk
@@ -133,6 +133,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
+    debug.cpurend.vsync=true \
+    debug.sf.disable_backpressure=0 \
     debug.sf.enable_gl_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     debug.sf.hw=1 \
