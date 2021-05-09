f04c42811e7dc5bb50f77a5233db1c77d7175e7c
kdrag0n <danny@kdrag0n.dev>
2021-05-09 10:48:23 +0530

    platina: Enable compositor background blurring
    This allows us to create a "glass blur" look throughout the system for
    improved visuals.
    
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>

--- properties.mk
+++ properties.mk
@@ -89,6 +89,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     vendor.bluetooth.soc=cherokee
 
+# Blur
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.bokeh.switch.lux=290 \
