e37fce38780931766cdf9d54701b8473d9c81caa
XsaviXander <22182013+XsaviXander@users.noreply.github.com>
2021-06-23 22:55:02 +0000

    platina: Enable freeform window management
    Change-Id: I2de7aab9d42c33989b53052266350a45982273db
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>

--- device.mk
+++ device.mk
@@ -166,6 +166,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.broadcastradio@1.0-impl
 
+# Freeform Multiwindow
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # fwk-detect
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
