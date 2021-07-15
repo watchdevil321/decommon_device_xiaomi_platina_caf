8a9c0ceb90dcb3fc3a46f7213a0f2366eab480db
Hanif Ardhani <hanif.ardhani98@gmail.com>
2021-07-15 21:23:56 +0700

    platina: Add option to build ANX or MIUI Camera
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>
    Change-Id: Ia0240d414cb59119715042c5f57a28d3012968fa

--- device.mk
+++ device.mk
@@ -31,6 +31,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# ANX Camera
+$(call inherit-product-if-exists, vendor/xiaomi/anx-camera/anx-camera.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl:32 \
@@ -260,6 +263,9 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+
+# MIUI Camera
+$(call inherit-product-if-exists, vendor/xiaomi/MiuiCamera/config.mk)
 
 # OMX
 PRODUCT_PACKAGES += \
