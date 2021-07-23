4df6d69560aa47114b570f2ede1f1b5e8d9eea8c
subhajeetmuhuri <subhajeet.muhuri@gmail.com>
2021-07-23 00:56:19 +0000

    platina: props: Enable GBoard rounded corners
    Change-Id: Ia2df4d2e0d8b4904d5137a60a67b3c5781f61482

--- properties.mk
+++ properties.mk
@@ -166,7 +166,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # GMS
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.com.google.ime.kb_pad_port_b=1
+    ro.com.google.ime.kb_pad_port_b=1 \
+    ro.com.google.ime.corner_key_r=35
 
 # Graphics
 ro.hardware.egl=adreno
