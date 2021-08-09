861083948521815215f74e476dc2c93f0b151ca7
Hanif Ardhani <hanif.ardhani98@gmail.com>
2021-08-09 11:31:31 +0700

    platina: Refactor board kernel cmdline
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>
    Change-Id: I331925919104c68348ad950ad156e0e0ae253feb

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -98,10 +98,23 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_platina
 TARGET_RECOVERY_DEVICE_MODULES := libinit_platina
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1 androidboot.configfs=true androidboot.usbcontroller=a800000.dwc3
-BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-BOARD_KERNEL_CMDLINE += kpti=off
-BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE := \
+    androidboot.configfs=true \
+    androidboot.console=ttyMSM0 \
+    androidboot.hardware=qcom \
+    androidboot.init_fatal_reboot_target=recovery \
+    androidboot.usbcontroller=a800000.dwc3 \
+    console=ttyMSM0,115200n8 \
+    earlycon=msm_serial_dm,0xc170000 \
+    ehci-hcd.park=3 \
+    kpti=off \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x37 \
+    service_locator.enable=1 \
+    swiotlb=1 \
+    user_debug=31
+
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
