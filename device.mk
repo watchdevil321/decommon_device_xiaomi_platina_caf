60dbc01f7c90b6976d82035406db87b237a95e38
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-12 20:36:05 +0200

    platina: Stop building android.hardware.broadcastradio HIDL impl
    * Nothing uses it and it isn't even in manifest
    
    Signed-off-by: Hanif Ardhani <hanif.ardhani98@gmail.com>
    Change-Id: I542a73bc453f174c5966b22e8787b2ecd9b40e66

--- device.mk
+++ device.mk
@@ -169,9 +169,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.3-service.clearkey
 
-PRODUCT_PACKAGES += \
-    android.hardware.broadcastradio@1.0-impl
-
 # Freeform Multiwindow
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
